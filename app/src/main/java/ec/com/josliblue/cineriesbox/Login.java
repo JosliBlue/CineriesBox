b7686ea98151397488e35b1914cb5495b41e82ed
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2024-05-25 13:36:33 -0500

    backend login y registro mejorados

--- app/src/main/java/ec/com/josliblue/cineriesbox/Login.java
+++ app/src/main/java/ec/com/josliblue/cineriesbox/Login.java
@@ -1,5 +1,6 @@
 package ec.com.josliblue.cineriesbox;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +10,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+
 public class Login extends AppCompatActivity {
 
     private EditText correo, clave;
     private Button btn_ingresar;
-    private TextView lbl_registarme;
+    private TextView lbl_registarme,lbl_sin_cuenta;
+    FirebaseAuth mAuth;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,36 +41,67 @@ public class Login extends AppCompatActivity {
             return insets;
         });
 
-        this.initView();
-    }
-
-    private void initView() {
         this.correo = findViewById(R.id.txt_Correo);
         this.clave = findViewById(R.id.txt_Clave);
-        this.btn_ingresar = findViewById(R.id.btn_Registrarme);
+        this.btn_ingresar = findViewById(R.id.btn_Ingresar);
+
+        mAuth = FirebaseAuth.getInstance();
+        intentarIngresar();
 
         this.lbl_registarme = findViewById(R.id.lbl_Registro);
-
-        this.btn_ingresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (correo.getText().toString().trim().isEmpty()) {
-                    Toast.makeText(Login.this, "Correo invalido", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (clave.getText().toString().trim().isEmpty()) {
-                    Toast.makeText(Login.this, "Contraseña invalida", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-        });
-
+        this.lbl_sin_cuenta = findViewById(R.id.lbl_SinCuenta);
         this.lbl_registarme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login.this, Registro.class));
-
+            }
+        });
+        this.lbl_sin_cuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Registro.class));
             }
         });
     }
+
+    private void intentarIngresar() {
+
+        this.btn_ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String correo_string = correo.getText().toString().trim();
+                String clave_string = clave.getText().toString().trim();
+
+                // Validación de campos
+                if (correo_string.isEmpty()) {
+                    Toast.makeText(Login.this, "Correo inválido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (clave_string.isEmpty()) {
+                    Toast.makeText(Login.this, "Contraseña inválida", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Autenticar con Firebase Authentication
+                mAuth.signInWithEmailAndPassword(correo_string, clave_string)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    //FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(Login.this, "Inciaste sesion", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    Toast.makeText(Login.this, "No inciaste sesion\nPrueba de nuevo. . .", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
 }
