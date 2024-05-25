426c966de5bd852953979015d52d529a47e57e91
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2024-05-25 11:56:53 -0500

    Backend registro terminado

--- app/src/main/java/ec/com/josliblue/cineriesbox/Registro.java
+++ app/src/main/java/ec/com/josliblue/cineriesbox/Registro.java
@@ -23,6 +23,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
     EditText correo, nombre, clave, claveConfirm;
@@ -49,7 +53,9 @@ public class Registro extends AppCompatActivity {
         this.claveConfirm = findViewById(R.id.txt_ConfirmaClave);
         this.btn_registrar = findViewById(R.id.btn_Registrarme);
         this.btn_returnLogin = findViewById(R.id.btn_returnLogin);
+
         mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         botonAtras();
         this.btn_registrar.setOnClickListener(new View.OnClickListener() {
@@ -58,36 +64,69 @@ public class Registro extends AppCompatActivity {
                 String correo_string, clave_string, nuevoNombre;
                 correo_string = String.valueOf(correo.getText());
                 clave_string = String.valueOf(clave.getText());
-                nuevoNombre = String.valueOf(nombre);
+                nuevoNombre = String.valueOf(nombre.getText());
+
+                // GUARDAR EL USUARIO EN AUTHENTICATION
                 if (comprobar()) {
                     mAuth.createUserWithEmailAndPassword(correo_string, clave_string)
                             .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (!task.isSuccessful()) {
-                                        Toast.makeText(Registro.this, "Usuario no creado. . .", Toast.LENGTH_SHORT).show();
-                                        return;
+                                    if (task.isSuccessful()) {
+                                        // ACTUALZIAR AL USUARIO CON SU NUEVO "DisplayName"
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        if (user != null) {
+                                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                                    .setDisplayName(nuevoNombre)
+                                                    .build();
+
+                                            user.updateProfile(profileUpdates)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                // GUARDAR EL USUARIO EN LA BD
+
+                                                                String userId = user.getUid();
+                                                                Map<String, Object> userMap = new HashMap<>();
+                                                                userMap.put("Identificador", correo_string);
+                                                                userMap.put("DisplayName", nuevoNombre);
+
+                                                                /*
+                                                                IMPORTANTE: ANTES DE QUERER USAR LA BD,
+                                                                EDITAR LAS REGLAS EN LA PAGINA DE FIREBASE PARA QUE CUALQUIERSA PUEDA INGRESAR
+                                                                DE LO CONTRARIO SIEMPRE DENEGARA LAS SOLICITUDES
+                                                                */
+                                                                db.collection("USUARIOS").document(userId)
+                                                                        .set(userMap)
+                                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                            @Override
+                                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                                if (task.isSuccessful()) {
+                                                                                    Toast.makeText(Registro.this, "Usuario creado :)\nInicia Sesion para continuar", Toast.LENGTH_SHORT).show();
+
+                                                                                    // Iniciar LoginActivity
+                                                                                    startActivity(new Intent(Registro.this, Login.class));
+                                                                                    finish(); // Opcional: cerrar la actividad actual para que el usuario no pueda volver con el botón "Atrás"
+                                                                                } else {
+                                                                                    Toast.makeText(Registro.this, "Error al guardar el usuario en la base de datos", Toast.LENGTH_SHORT).show();
+                                                                                }
+                                                                            }
+                                                                        });
+                                                            } else {
+                                                                Toast.makeText(Registro.this, "Error al colocar el nombre del usuario", Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        }
+                                                    });
+                                        }
+
+                                    } else {
+                                        Toast.makeText(Registro.this, "Usuario ya existente. . .", Toast.LENGTH_SHORT).show();
                                     }
                                 }
                             });
-                    FirebaseUser user = mAuth.getCurrentUser();
 
-                    if (user != null) {
-                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                .setDisplayName(nuevoNombre)
-                                .build();
 
-                        user.updateProfile(profileUpdates)
-                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (!task.isSuccessful()) {
-                                            Toast.makeText(Registro.this, "Error al colocar el nombre del usuario", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                });
-                    }
-                    Toast.makeText(Registro.this, "Usuario creado :)", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -101,7 +140,7 @@ public class Registro extends AppCompatActivity {
         }
 
         // PONER CONTROL DE CORREO UNICO
-
+        // parece q este control lo hace automatico firebase
         // PONER CONTROL DE CONTRASEÑA MAS DE 6 CARACTERES
         if (this.clave.getText().toString().trim().length() <= 5) {
             Toast.makeText(this, "La contraseña debe ser mas de 6 caracteres. . .", Toast.LENGTH_SHORT).show();
