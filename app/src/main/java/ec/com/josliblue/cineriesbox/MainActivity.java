8ac56b1cd0d9fd353a577db6349de9dac5c12cf2
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2024-05-24 23:19:58 -0500

    Frontend login y registro implementados

--- app/src/main/java/ec/com/josliblue/cineriesbox/MainActivity.java
+++ app/src/main/java/ec/com/josliblue/cineriesbox/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         // Lanzar LoginActivity
         Intent intent = new Intent(MainActivity.this, Login.class);
--- app/src/main/java/ec/com/josliblue/cineriesbox/Registro.java
+++ app/src/main/java/ec/com/josliblue/cineriesbox/Registro.java
@@ -1,6 +1,11 @@
 package ec.com.josliblue.cineriesbox;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Registro extends AppCompatActivity {
 
+    ImageButton btn_returnLogin;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.btn_returnLogin = findViewById(R.id.btn_returnLogin);
+        this.btn_returnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registro.this, Login.class));
+            }
+        });
+
     }
 }
