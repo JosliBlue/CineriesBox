aa9568621a46a10a2e6b172ef7278015520949c0
JosliBlue <eliassaulojoslimuyu@hotmail.com>
2024-06-04 14:17:38 -0500

    logueo automatico implementado

--- app/src/main/java/Main/ActividadMain.java
+++ app/src/main/java/Main/ActividadMain.java
@@ -6,29 +6,38 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.google.firebase.auth.FirebaseUser;
+
 import Auth.ActividadLogin;
+import Utilidades.BDFirebase;
+import ec.com.josliblue.cineriesbox.R;
 
 public class ActividadMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-         */
 
         //Puse la aplicacion predeterminada en modo oscuro
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
+        //aqui pongo a la barra de navegacion inferior del color de mi barra de navegacion de mi app
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.dark_main_color));
+        //este de aca abajo es para poner la barra de navegacion de un color puesto por mi
+        //window.setStatusBarColor(getResources().getColor(R.color.main_color));
+
         // Lanzar LoginActivity
-        Intent intent = new Intent(ActividadMain.this, ActividadLogin.class);
-        startActivity(intent);
-        finish();
+        FirebaseUser currentUser = BDFirebase.getUsuarioActual();
+        if (currentUser != null) {
+            // Usuario ya ha iniciado sesión, redirigir a ActividadPantallaPrincipal
+            Intent intent = new Intent(ActividadMain.this, ActividadPantallaPrincipal.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Usuario no ha iniciado sesión, permanecer en ActividadMain o redirigir a ActividadLogin
+            Intent intent = new Intent(ActividadMain.this, ActividadLogin.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/Main/ActividadPantallaPrincipal.java
+++ app/src/main/java/Main/ActividadPantallaPrincipal.java
@@ -3,11 +3,11 @@ package Main;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,8 +23,7 @@ import Fragmentos.FragmentoPrincipal;
 import ec.com.josliblue.cineriesbox.R;
 
 public class ActividadPantallaPrincipal extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
-    private FrameLayout frameLayout;
+    private BottomNavigationView menuNavegacionApp;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -34,42 +33,47 @@ public class ActividadPantallaPrincipal extends AppCompatActivity {
         setContentView(R.layout.actividad_pantalla_principal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, v.getPaddingBottom());
             return insets;
         });
 
-        this.frameLayout = findViewById(R.id.EspacioApp);
-        this.bottomNavigationView = findViewById(R.id.EspacioMenu);
+        //Puse la aplicacion predeterminada en modo oscuro
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        //aqui pongo a la barra de navegacion inferior del color de mi barra de navegacion de mi app
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.dark_main_color));
+        //este de aca abajo es para poner la barra de navegacion de un color puesto por mi
+        //window.setStatusBarColor(getResources().getColor(R.color.main_color));
+
+        this.menuNavegacionApp = findViewById(R.id.EspacioMenu);
+
+        menuNavegacionApp.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
                 int itemId = menuItem.getItemId();
                 if (itemId == R.id.navPrincipal) {
-                    cargarFragmento(new FragmentoPrincipal(),false);
+                    cargarFragmento(new FragmentoPrincipal(), false);
                 } else if (itemId == R.id.navBuscar) {
-                    cargarFragmento(new FragmentoBuscar(),false);
+                    cargarFragmento(new FragmentoBuscar(), false);
                 } else if (itemId == R.id.navPerfil) {
-                    cargarFragmento(new FragmentoPerfil(),false);
+                    cargarFragmento(new FragmentoPerfil(), false);
                 }
                 return true;
             }
         });
-        cargarFragmento(new FragmentoPrincipal(),true);
+        cargarFragmento(new FragmentoPrincipal(), true);
     }
 
-    private void cargarFragmento(Fragment fragmento,boolean isAppInitialized) {
+    private void cargarFragmento(Fragment fragmento, boolean isAppInitialized) {
         FragmentManager fragmentoManager = getSupportFragmentManager();
         FragmentTransaction fragmentoTransaction = fragmentoManager.beginTransaction();
 
-        if(isAppInitialized){
+        if (isAppInitialized) {
             fragmentoTransaction.add(R.id.EspacioApp, fragmento);
-        }else{
+        } else {
             fragmentoTransaction.replace(R.id.EspacioApp, fragmento);
         }
-
-
         fragmentoTransaction.commit();
     }
 }
